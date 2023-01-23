f3e520a011b25c966bc025b43971c177aaeef4f1
dipankar121 <94380559+dipankar121@users.noreply.github.com>
2023-01-23 11:14:56 +0530

    2 Coding Questions

---
+++ 100CodingQuestions.js
@@ -0,0 +1,36 @@
+// 1. How to define a class withy properties and methods?
+// class Car {
+//   constructor(model, name) {
+//     (this.model = model), (this.name = name);
+//   }
+
+//   start() {
+//     console.log(`${this.name} Started`);
+//   }
+
+//   end() {
+//     console.log(`${this.name} Stopped`);
+//   }
+// }
+
+// bmw = new Car(620, "sports_edition");
+// bmw.start();
+
+// 2. how to implement class inheritance?
+// class ElectricCar extends Car {
+//   start() {
+//     super.start();
+//   }
+//   end() {
+//     super.end();
+//   }
+
+//   speed() {
+//     console.log("Very Fast");
+//   }
+// }
+
+// let evcar = new ElectricCar("Tesla", "a320");
+// evcar.start();
+// evcar.end();
+// evcar.speed();
